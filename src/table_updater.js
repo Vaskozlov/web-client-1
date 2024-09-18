d74ff0c66bf8412b3305c51d3adaaba85a3983fc
vaskozlov <vaskozlov3940@gmail.com>
2024-09-18 11:45:20 +0300

    feat: execution time formatting

--- src/table_updater.js
+++ src/table_updater.js
@@ -1,9 +1,24 @@
 const is_in_area_table = document.getElementById("is_in_area_table")
 let current_cell = 1;
 
+function formatTimeFromNs(time_ns) {
+    const time_us = time_ns / 1000;
+    const time_ms = time_us / 1000;
+
+    if (time_ms > 1) {
+        return `${time_ms.toFixed(2)}ms`;
+    }
+
+    if (time_us > 1) {
+        return `${time_us.toFixed(2)}us`;
+    }
+
+    return `${time_ns}ns`;
+}
+
 export function insertToTable(x, y, r, is_in_area, time_ns) {
     const row = is_in_area_table.insertRow(current_cell);
-    
+
     let x_cell = row.insertCell(0);
     let y_cell = row.insertCell(1);
     let r_cell = row.insertCell(2);
@@ -14,9 +29,7 @@ export function insertToTable(x, y, r, is_in_area, time_ns) {
     y_cell.innerHTML = y.toString();
     r_cell.innerHTML = r.toString();
     is_in_area_cell.innerHTML = is_in_area.toString();
-    time_ns_cell.innerHTML = new Intl.DurationFormat("en").format({
-        nanoseconds: time_ns
-    });
+    time_ns_cell.innerHTML = formatTimeFromNs(time_ns);
 
     ++current_cell;
 }
