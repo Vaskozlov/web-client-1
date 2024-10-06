d0d6ab2bcfe147259280d3a2a87ebc6f55768164
vaskozlov <vaskozlov3940@gmail.com>
2024-10-06 12:45:06 +0300

    feat: more generalized arguments for insertIntoTable function

--- src/is_in_area_table.js
+++ src/is_in_area_table.js
@@ -6,15 +6,13 @@ const is_in_area_table = document.getElementById("is_in_area_table")
 export function insertIntoTable(x, y, r, is_in_area, time_ns) {
     const row = is_in_area_table.insertRow(current_cell);
 
-    const x_cell = row.insertCell(0);
-    const y_cell = row.insertCell(1);
-    const r_cell = row.insertCell(2);
+    for (let i = 0; i !== 3; ++i) {
+        row.insertCell(i).innerHTML = arguments[i];
+    }
+
     const is_in_area_cell = row.insertCell(3);
     const time_ns_cell = row.insertCell(4);
 
-    x_cell.innerHTML = x.toString();
-    y_cell.innerHTML = y.toString();
-    r_cell.innerHTML = r.toString();
     is_in_area_cell.innerHTML = is_in_area ? "yes" : "no";
     time_ns_cell.innerHTML = formatTimeNs(time_ns);
 
